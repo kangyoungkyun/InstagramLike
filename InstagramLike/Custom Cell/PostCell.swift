770b6a738183e14cf4549aaff45632e63051425d
MacBookPro <macbookpro@MacBookProui-MacBook-Pro.local>
2018-03-09 12:15:02 +0900

    PostCell delegate 구현 prototype 메서드 생성 ->  FeedController에 위임 -> 관련 메소드 구현 (MVC), 좋아요 누르고 CollectoinView 아래로 스크롤 하면 db에는 정상 반영되지만 view에서 초기화 되는 문제 해결(button이 눌린 위치를 이용해서 cell의 index 구한 후 해당 배열의 변수 변경)

--- InstagramLike/Custom Cell/PostCell.swift
+++ InstagramLike/Custom Cell/PostCell.swift
@@ -4,12 +4,25 @@
 //
 //  Created by MacBookPro on 2018. 3. 7..
 //  Copyright © 2018년 MacBookPro. All rights reserved.
-//
+// 좋아요를 눌렀을때 PostCell에서는 정확한 개수가 반영이된다
+
+//cell for row at -> postcell에 좋아요 버튼 클릭함수 안에 delegate구현 -> 다시 컨트롤러에 extension POstdelegate 안에 actoin 함수
+
+//*델리게이트 작동 순서
+//post cell에서 delegate?.likeAction 작동
+//FeedViewController - likAction 함수
+
+//위임을 받으려면 controller 부분에서 이 메서드를 반드시 구현
+protocol PostCellDelegate: class {
+    func likeActoin(cell:PostCell)
+    func unlikeAction(cell:PostCell)
+}
 
 import UIKit
 import Firebase
 class PostCell: UICollectionViewCell {
-    
+    //델리게이트 변수
+    weak var delegate: PostCellDelegate?
     
     @IBOutlet weak var postImage: UIImageView!
     
@@ -23,113 +36,20 @@ class PostCell: UICollectionViewCell {
     
     var postID: String?
     
-    
-    
+
     //좋아요 버튼이 눌렀을 때
     @IBAction func likePressed(_ sender: Any) {
-        self.likeBtn.isEnabled = false
-        //위치
-        let ref = Database.database().reference()
-        //랜덤 키
-        let keyToPost = ref.child("posts").childByAutoId().key
         
-        //포스트-> 포스트 id 에서 데이터 가져오기
-        ref.child("posts").child(self.postID!).observeSingleEvent(of: .value) { (snapshot) in
-            //데이터가 존재하면
-            if let posts = snapshot.value as? [String:AnyObject]{
-                
-                print("좋아요 posts: \(posts)")
-                
-                //peopleWhoLike 폴더에 타입이 key: value 인 데이터 넣기
-                let updateLikes: [String:Any] = ["peopleWhoLike/\(keyToPost)": Auth.auth().currentUser?.uid as Any]
-                
-                print("updateLikes \(updateLikes)")
-                
-                //포스트 -> 포스트 id에 업데이트(클릭한 사람의 pk 값 넣어주기)
-                ref.child("posts").child(self.postID!).updateChildValues(updateLikes, withCompletionBlock: { (error, reff) in
-                    //업데이트 성공하면
-                    if error == nil{
-                        //포스트-> 포스트ID에서 데이터 가져오기
-                        ref.child("posts").child(self.postID!).observeSingleEvent(of: .value, with: { (snap) in
-                            //데이터가 존재하면
-                            if let properties = snap.value as? [String:AnyObject]{
-                                //peopleWhoLike 폴더에 있는 데이터들 담기
-                                if let likes = properties["peopleWhoLike"] as? [String:AnyObject]{
-                                    //배열 개수 파악
-                                    let count = likes.count
-                                    print("좋아요 개수 몇개 \(count)")
-                                    //할당해주기
-                                    self.likeLabel.text = "\(count) Likes"
-                                    //최종개수 업데이트
-                                    let update = ["likes" : count]
-                                    ref.child("posts").child(self.postID!).updateChildValues(update)
-                                    
-                                    self.likeBtn.isHidden = true
-                                    self.unlikeBtn.isHidden = false
-                                    self.likeBtn.isEnabled = true
-                                    
-                                }
-                            }
-                        })
-                    }
-                })
-            }
-        }
-        ref.removeAllObservers()
+        print("post cell에서 delegate?.likeAction 작동")
+        //델리게이트 이용 - cell에 있는 likeAction 함수를 눌렀는데 controller에 있는 likeActoin 함수가 작동한다 이유는  delegate를 통해서 위임을 했기때문!
+        delegate?.likeActoin(cell: self)
     }
-    
-    
     
     
     //싫어요 버튼 눌렀을 때
     @IBAction func unlikePressed(_ sender: Any) {
-        self.unlikeBtn.isEnabled = false
-        //위치
-        let ref = Database.database().reference()
-        //포스트-> 포스트id
-        ref.child("posts").child(self.postID!).observeSingleEvent(of: .value) { (snapshot) in
-            // 포스트 폴더에 데이터 가져오기
-            if let properties = snapshot.value as? [String: AnyObject]{
-                //포스트 -> peopleWhoLike
-                if let peopleWhoLike = properties["peopleWhoLike"] as? [String:AnyObject]{
-                    //키, 좋아요 누른 유저 id
-                    for(id, person) in peopleWhoLike{
-                        // 좋아요 누른 사람아이디 == 지금의 내 아이디
-                        if person as? String == Auth.auth().currentUser?.uid{
-                            //포스트 폴더 -> postId -> peopleWhoLike -> id 삭제
-                            ref.child("posts").child(self.postID!).child("peopleWhoLike").child(id).removeValue(completionBlock: { (error, rer) in
-                                //에러가 안나면
-                                if error == nil{
-                                    //포스트 폴더에서 데이터 가져오기
-                                    ref.child("posts").child(self.postID!).observeSingleEvent(of: .value, with: { (snapshot) in
-                                        //포스트 데이터 가져오기
-                                        if let prop = snapshot.value as? [String:AnyObject]{
-                                            //포스트 폴더에서 peopleWhoLike 키에 담긴 {key:value} 가져오기
-                                            if let likes = prop["peopleWhoLike"] as? [String:AnyObject]{
-                                                //개수 파악
-                                                let count = likes.count
-                                                //표시해주기
-                                                self.likeLabel.text = "\(count) Likes"
-                                                //포스트 -> 포스트 id -> likes 키 값만 바꾸기
-                                                ref.child("posts").child(self.postID!).updateChildValues(["likes": count])
-                                                
-                                            }else{
-                                                self.likeLabel.text = "0 Likes"
-                                                ref.child("posts").child(self.postID!).updateChildValues(["likes" : 0])
-                                            }
-                                        }
-                                    })
-                                }
-                            })
-                            self.likeBtn.isHidden = false
-                            self.unlikeBtn.isHidden = true
-                            self.unlikeBtn.isEnabled = true
-                            break
-                        }
-                    }
-                }
-            }
-        }
-         ref.removeAllObservers()
+         print("post cell에서 delegate?.unlikeAction 작동")
+        //델리게이트 이용
+        delegate?.unlikeAction(cell: self)
     }
 }
--- InstagramLike/View Controllers/FeedViewController.swift
+++ InstagramLike/View Controllers/FeedViewController.swift
@@ -19,10 +19,6 @@ class FeedViewController: UIViewController, UICollectionViewDelegate, UICollecti
     
     @IBOutlet weak var collectionview: UICollectionView!
     
-    
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("feedViewController viewdidload")
@@ -67,16 +63,16 @@ class FeedViewController: UIViewController, UICollectionViewDelegate, UICollecti
                             
                             for(_, post) in postsSnapt!{
                                 
-                                print("post 나와랏: \(post)")
-                                print("post[userID] 나와랏: \(post["userId"])")
+                                //print("post 나와랏: \(post)")
+                                //print("post[userID] 나와랏: \(post["userId"])")
                                 //post에서 userId에 값이 있다면
                                 if let userID = post["userId"] as? String{
                                     //following 배열에 저장된 uid들을 가져온다.
                                     for each in self.following {
-                                        print("each 나와랏: \(each)")
+                                        //print("each 나와랏: \(each)")
                                         //같은 값이 있다면
                                         if each == userID{
-                                            print("each == userID 나와랏: \(userID)")
+                                            //print("each == userID 나와랏: \(userID)")
                                             //데이터를 담을 객체 생성
                                             let posst = Post()
                                             //nil 확인
@@ -88,32 +84,32 @@ class FeedViewController: UIViewController, UICollectionViewDelegate, UICollecti
                                                 posst.postID = postID
                                                 posst.pathToImage = pathToImage
                                                 
-                                                //좋아요 버튼 누른 사람 담아주기
+                                                //좋아요 버튼 누른 사람 배열에 담아주기
+                                                print("fetchPosts- 좋아요 버튼 누른 사람 배열에 담아주기")
                                                 if let people = post["peopleWhoLike"] as? [String:AnyObject]{
                                                     for(_,person) in people{
+                                                        print("fetchPosts- 좋아요 누른 사람의 아이디는? \(person)")
+                                                         print("fetchPosts- 좋아요 누른 사람의 개수는? \(posst.peopleWhoLike.count)")
                                                         posst.peopleWhoLike.append(person as! String)
                                                     }
                                                 }
                                                 
                                                 //객체를 배열에 하나씩 담아준다.
                                                 self.posts.append(posst)
-                                                print("posts 하나 넣었따 개수는? \(self.posts.count)")
+                                                print("fetchPosts -posts 하나 넣었다 개수는? \(self.posts.count)")
                                             }
                                         }
                                     }
                                    
-                                     //DispatchQueue.main.async(execute: {
+                                     DispatchQueue.main.async(execute: {
                                        self.collectionview.reloadData()
-                                    //})
+                                    })
                                 }
                             }
                         })
                     }
                 }
             }
-           // DispatchQueue.main.async(execute: {
-            //   self.collectionview.reloadData()
-            //})
         }
         ref.removeAllObservers()
     }
@@ -124,6 +120,7 @@ class FeedViewController: UIViewController, UICollectionViewDelegate, UICollecti
     //컬랙션 뷰 개수
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         print("컬렉션 뷰 개수 : \(posts.count)")
+        print("컬렉션 뷰 section : \(section)")
         return posts.count
     }
     
@@ -135,14 +132,21 @@ class FeedViewController: UIViewController, UICollectionViewDelegate, UICollecti
         cell.postImage.downloadImage(from: self.posts[indexPath.row].pathToImage)
         cell.authorLabel.text = self.posts[indexPath.row].author
         cell.likeLabel.text = "\(self.posts[indexPath.row].likes!) Likes"
-        
-        print("좋아요개수 좋아요 개수 cell for row at : \(String(describing: cell.likeLabel.text))")
+        //print(" cell for row at 좋아요개수 : \(self.posts[indexPath.row].likes!)")
         
         //cell에 포스트 아이디 넣어주기
         cell.postID = self.posts[indexPath.row].postID
         
+        //controller가 cell의 델리게이트가 되고 싶어한다. 그래서 PostCell에서 구현한 prototype 메서드를 구현해줘야 한다.!!
+        cell.delegate = self
+        
+         print("cellForItemAt - cell.delegate = self")
+        
+         print("cellForItemAt- 좋아요 누른 사람의 배열 개수는? \(self.posts[indexPath.row].peopleWhoLike.count)")
+        
         //Post 데이터 모델에서 peopleWhoLike 변수 배열에서 user 가져와서 있으면 like 버튼 hidden
         for person in self.posts[indexPath.row].peopleWhoLike{
+            //print("cell for row at 좋아요 누른 사람: \(person)")
             if person == Auth.auth().currentUser?.uid{
                 cell.likeBtn.isHidden = true
                 cell.unlikeBtn.isHidden = false
@@ -154,4 +158,180 @@ class FeedViewController: UIViewController, UICollectionViewDelegate, UICollecti
         
     }
     
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("didSelectItemAt \(indexPath.row) 번째 cell이 선택되었습니다")
+    }
+}
+
+
+extension FeedViewController : PostCellDelegate{
+    //좋아요 버튼 눌렀을 때
+    func likeActoin(cell: PostCell) {
+        print("FeedViewController - likAction 함수")
+        
+        //1. 파이어 베이스에서 좋아요 숫자 삽입
+        cell.likeBtn.isEnabled = false
+        //위치
+        let ref = Database.database().reference()
+        //랜덤 키
+        let keyToPost = ref.child("posts").childByAutoId().key
+        
+        //포스트-> 포스트 id 에서 데이터 가져오기
+        ref.child("posts").child(cell.postID!).observeSingleEvent(of: .value) { (snapshot) in
+            //데이터가 존재하면
+            if let posts = snapshot.value as? [String:AnyObject]{
+                
+                print("좋아요 posts: \(posts)")
+                
+                //peopleWhoLike 폴더에 타입이 key: value 인 데이터 넣기
+                let updateLikes: [String:Any] = ["peopleWhoLike/\(keyToPost)": Auth.auth().currentUser?.uid as Any]
+                
+                print("updateLikes \(updateLikes)")
+                
+                //포스트 -> 포스트 id에 업데이트(클릭한 사람의 pk 값 넣어주기)
+                ref.child("posts").child(cell.postID!).updateChildValues(updateLikes, withCompletionBlock: { (error, reff) in
+                    //업데이트 성공하면
+                    if error == nil{
+                        //포스트-> 포스트ID에서 데이터 가져오기
+                        ref.child("posts").child(cell.postID!).observeSingleEvent(of: .value, with: { (snap) in
+                            //데이터가 존재하면
+                            if let properties = snap.value as? [String:AnyObject]{
+                                //peopleWhoLike 폴더에 있는 데이터들 담기
+                                if let likes = properties["peopleWhoLike"] as? [String:AnyObject]{
+                                    //배열 개수 파악
+                                    let count = likes.count
+                                    //print("좋아요 개수 몇개 \(count)")
+                                    
+                                    
+                                    //버튼이 눌려진 cell의 index를 위치를 이용해서 구하는 방법!
+                                    let point : CGPoint = cell.likeBtn.convert(CGPoint.zero, to:self.collectionview)
+                                    let indexPath = self.collectionview!.indexPathForItem(at: point)
+                                    
+                                    let index = indexPath?.row as! Int
+                                
+                                    //print("과연4 \(indexPath?.row as! Int)")
+                                    
+                                    //좋아요 누른 해당 배열 인덱스에 접근해서 likes 변수를 바꿔준다. 그래야
+                                    //collection view를 스크롤 해도 그대로 표시가 된다.!
+                                    self.posts[index].likes = count
+                                    
+                                    //할당해주기
+                                    cell.likeLabel.text = "\(count) Likes"
+                                    //최종개수 업데이트
+                                    let update = ["likes" : count]
+                                    ref.child("posts").child(cell.postID!).updateChildValues(update)
+                                    cell.likeBtn.isHidden = true
+                                    cell.unlikeBtn.isHidden = false
+                                    cell.likeBtn.isEnabled = true
+                                    
+                                }
+                            }
+                        })
+                    }
+                })
+            }
+        }
+        ref.removeAllObservers()
+
+        //2. 컬렉션뷰 수정
+        //self.collectionview.reloadData()
+        
+    }
+    
+    
+    //안좋아요 버튼 눌렀을 때
+    func unlikeAction(cell: PostCell) {
+        
+        //버튼이 눌려진 cell의 index를 위치를 이용해서 구하는 방법!
+        let point : CGPoint = cell.likeBtn.convert(CGPoint.zero, to:self.collectionview)
+        let indexPath = self.collectionview!.indexPathForItem(at: point)
+        let index = indexPath?.row as! Int
+        
+         print("FeedViewController - unlikeAction 함수")
+        //1. 파이어 베이스에서 좋아요 숫자 삽입
+        cell.unlikeBtn.isEnabled = false
+        //위치
+        let ref = Database.database().reference()
+        //포스트-> 포스트id
+        ref.child("posts").child(cell.postID!).observeSingleEvent(of: .value) { (snapshot) in
+            // 포스트 폴더에 데이터 가져오기
+            if let properties = snapshot.value as? [String: AnyObject]{
+                //포스트 -> peopleWhoLike
+                if let peopleWhoLike = properties["peopleWhoLike"] as? [String:AnyObject]{
+                    //키, 좋아요 누른 유저 id
+                    for(id, person) in peopleWhoLike{
+                        // 좋아요 누른 사람아이디 == 지금의 내 아이디
+                        if person as? String == Auth.auth().currentUser?.uid{
+                            //포스트 폴더 -> postId -> peopleWhoLike -> id 삭제
+                            ref.child("posts").child(cell.postID!).child("peopleWhoLike").child(id).removeValue(completionBlock: { (error, rer) in
+                                //에러가 안나면
+                                if error == nil{
+                                    //포스트 폴더에서 데이터 가져오기
+                                    ref.child("posts").child(cell.postID!).observeSingleEvent(of: .value, with: { (snapshot) in
+                                        //포스트 데이터 가져오기
+                                        if let prop = snapshot.value as? [String:AnyObject]{
+                                            //포스트 폴더에서 peopleWhoLike 키에 담긴 {key:value} 가져오기
+                                            if let likes = prop["peopleWhoLike"] as? [String:AnyObject]{
+                                                //개수 파악
+                                                let count = likes.count
+                                                
+          
+                                                
+                                                //좋아요 누른 해당 배열 인덱스에 접근해서 likes 변수를 바꿔준다. 그래야
+                                                //collection view를 스크롤 해도 그대로 표시가 된다.!
+                                                self.posts[index].likes = count
+ 
+                                                
+                                                //표시해주기
+                                                cell.likeLabel.text = "\(count) Likes"
+                                                //포스트 -> 포스트 id -> likes 키 값만 바꾸기
+                                                ref.child("posts").child(cell.postID!).updateChildValues(["likes": count])
+                                                
+                                            }else{
+                                                self.posts[index].likes = 0
+                                                cell.likeLabel.text = "0 Likes"
+                                                ref.child("posts").child(cell.postID!).updateChildValues(["likes" : 0])
+                                            }
+                                        }
+                                    })
+                                }
+                            })
+                            cell.likeBtn.isHidden = false
+                            cell.unlikeBtn.isHidden = true
+                            cell.unlikeBtn.isEnabled = true
+                            break
+                        }
+                    }
+                }
+            }
+        }
+        ref.removeAllObservers()
+        
+        //2. 컬렉션뷰 수정
+         //self.collectionview.reloadData()
+    }
+    
+    
+    
+    
+}
+
+
+extension UICollectionView {
+    
+    var centerPoint : CGPoint {
+        
+        get {
+            return CGPoint(x: self.center.x + self.contentOffset.x, y: self.center.y + self.contentOffset.y);
+        }
+    }
+    
+    var centerCellIndexPath: IndexPath? {
+        
+        if let centerIndexPath = self.indexPathForItem(at: self.centerPoint) {
+            return centerIndexPath
+        }
+        return nil
+    }
 }
